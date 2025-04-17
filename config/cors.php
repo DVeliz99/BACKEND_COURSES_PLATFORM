f804bdfb021ff1b369803d92a0befc9139b85c30
Dario Veliz <velizalexander99@gmail.com>
2025-04-17 17:25:06 -0600

    (fix: '/' removido de allowed_origins)

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://courses-platform-frontend-nine.vercel.app/'],
+    'allowed_origins' => ['https://courses-platform-frontend-nine.vercel.app'],
 
     'allowed_origins_patterns' => [],
 

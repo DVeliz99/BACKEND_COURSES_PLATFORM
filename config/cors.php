e265fdf7c2f6fc23f5f4f14a7b32a6b93f2ee649
DVeliz99 <velizalexander99@gmail.com>
2025-08-11 21:10:47 -0600

    --amend

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://courses-platform-frontend-beta.vercel.app'],
+    'allowed_origins' => ['https://courses-platform-frontend-two.vercel.app/login'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
